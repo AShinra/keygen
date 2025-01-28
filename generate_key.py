020f29cb480e23a1bac114cd0e500ef8be48e341
Athan <jonpuray@gmail.com>
2025-01-28 09:40:15 +0800

    added user philip

--- generate_key.py
+++ generate_key.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import streamlit_authenticator as stauth
 
 
-names = ['Jonathan Puray', 'Sheya Espaldon', 'Racquel Trillana', 'Andrea Joi Centeno', 'Ayessa Juraine Mancera', 'Ariza Alarcon', 'Paula Poblete', 'Mara Laya', 'Maria Anna Mae Paruan', 'Alvin Deocareza', 'Ayrone Masilungan', 'Christalline Llarena', 'Fritz Acebes']
-usernames = ['athan', 'sheyaesp', 'kelly', 'axcnn', 'amancera', 'amalarcon', 'pau', 'mclaya', 'amaeparuan', 'adeocareza', 'vashy', 'talline', 'fritz']
-passwords = ['adminako', '123mmi', 'mmi12345', '060895', 'ajmancera', 'amalarcon', 'mmitool', 'mara', '12345mmi', 'alvinmmi', 'stronger', 'tallinellarena082923', 'prets098']
+names = ['Jonathan Puray', 'Sheya Espaldon', 'Racquel Trillana', 'Andrea Joi Centeno', 'Ayessa Juraine Mancera', 'Ariza Alarcon', 'Paula Poblete', 'Mara Laya', 'Maria Anna Mae Paruan', 'Alvin Deocareza', 'Ayrone Masilungan', 'Christalline Llarena', 'Fritz Acebes', 'Christian Philip Lendio']
+usernames = ['athan', 'sheyaesp', 'kelly', 'axcnn', 'amancera', 'amalarcon', 'pau', 'mclaya', 'amaeparuan', 'adeocareza', 'vashy', 'talline', 'fritz', 'philip']
+passwords = ['adminako', '123mmi', 'mmi12345', '060895', 'ajmancera', 'amalarcon', 'mmitool', 'mara', '12345mmi', 'alvinmmi', 'stronger', 'tallinellarena082923', 'prets098', 'qwertyuioP123$%']
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
